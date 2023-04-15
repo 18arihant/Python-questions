f0a81ef67e6796eb5f8178a603888aeaf3ca6731
18arihant <arihant18082003@gmail.com>
2023-04-15 11:54:08 +0530

    question13

---
+++ 13.py
@@ -0,0 +1,11 @@
+#star patter
+# *
+# * *
+# * * *
+a=int(input("Enter number of rows to which you want star patters:"))
+for i in range(0,a):
+    for j in range(0,i+1):
+        print("*" ,end=' ')
+    print("\n")
+    
+ 
