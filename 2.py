0d5d457240b8d1a101a56f9241d608e6ecf1b2b7
18arihant <arihant18082003@gmail.com>
2023-04-15 09:47:40 +0530

    question2

---
+++ 2.py
@@ -0,0 +1,6 @@
+#to find a remainder when a number is divided by a numbrer.
+
+a=int(input("Enter a number:"))
+b=int(input("Enter a number to get divided by:"))
+remainder= a%b
+print("The remainder when",a,"divided by",b,"is:",remainder)
