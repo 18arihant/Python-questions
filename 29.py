cc6d49899c5556fdb50d13bc722a0ca977207176
18arihant <arihant18082003@gmail.com>
2023-05-05 11:51:27 +0530

    question 29

--- 29.py
+++ 29.py
@@ -4,33 +4,27 @@ class coffee:
     def menue(self):
         print("The choice are: latte, expresso and normal coffee")
         self.a=input("Enter your choice:")
+        self.b=int(input("Plese enter the quantity:"))
 
-    # def __init__(self,d,e,f):
-    #     self.d=d
-    #     self.e=e
-    #     self.f=f
-    
     def quantity(self,d,e,f):
-         self.d=d
-         self.e=e
-         self.f=f
-         print("The quantity of water sugar and coffee is",d, "gms",e, "grms",f, "gms")
-
-
-
+        self.d=d
+        self.e=e
+        self.f=f
+        print("The quantity of water sugar and coffee is",d, "gms",e, "grms",f, "gms")
+    
     def amount(self):
         if self.a=="latte":
-            print("pay Rs 60")
+            print("pay Rs", 60*self.b)
         elif self.a=="expresso":
-            print("pay Rs 70")
+            print("pay Rs", 70*self.b)
         elif self.a=="coffee":
-            print("pay Rs 50")
+            print("pay Rs",50*self.b)
         else:
             print("wrong choice")
 
 arihant=coffee()
 arihant.menue()
-arihant.amount()
 arihant.quantity(50,50,50)
+arihant.amount()
 
 
