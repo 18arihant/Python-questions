1fd8250f3a65384c9bf60d1de1935efcdfad2d38
18arihant <arihant18082003@gmail.com>
2023-05-08 20:08:30 +0530

    question 6

--- LeetCode Questions/6.py
+++ LeetCode Questions/6.py
@@ -2,32 +2,18 @@
 
 class Solution:
     def sum(self,nums):
-        if len(nums)>1:
-            leftsum=[0]
-        else:
-        rightsum=[0]
-        answer=[]
-        left=0
-        right=0
-        for i in range(0,len(nums)-1):
-            left+=nums[i]
-            leftsum.append(left)
-        # print(leftsum)
-        for i in range(len(nums)-1,-1,-1):
-            right+=nums[i]
-            rightsum.insert(0,right)
-            if len(rightsum)==len(nums):
-               break
-        # print(rightsum)
-        for i in range(len(nums)):
-            if leftsum[i]>rightsum[i]:
-                answer.append((leftsum[i]-rightsum[i]))
-            else:
-                answer.append((rightsum[i]-leftsum[i]))
-        return answer
+       answer=[]
+       left=0
+       right=sum(nums)
+       for i in nums:
+           right-=i
+           answer.append(abs(left-right))
+           left+=i
+       return answer
+    
 
 a=Solution()
-print(a.sum([10,4,8,3]))
+print(a.sum([1]))
 
             
 
