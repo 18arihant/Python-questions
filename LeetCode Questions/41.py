c96c63a4f7cd4fc3c0d165170c7786b85df4e66d
18arihant <arihant18082003@gmail.com>
2023-06-19 23:06:32 +0530

    question 41

---
+++ LeetCode Questions/41.py
@@ -0,0 +1,13 @@
+# 2413. Smallest Even Multiple
+class solution():
+    def multiple(self,n):
+        if n%2==0:
+            return n
+        else:
+            return 2*n
+            
+
+a=solution()
+print(a.multiple(77))
+
+            
