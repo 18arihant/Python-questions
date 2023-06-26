674bddca8b86b1e1d8edef7d79f3f06cf61f49e6
18arihant <arihant18082003@gmail.com>
2023-06-26 19:48:38 +0530

    question  47

--- LeetCode Questions/47.py
+++ LeetCode Questions/47.py
@@ -3,7 +3,7 @@
 class Solution():
     def commonFactors(self, a, b):
         c=0
-        for i in range(1,max(a,b)+1):
+        for i in range(1,min(a,b)+1):
             if(a%i==0 and b%i==0):
                 c+=1
         return c
