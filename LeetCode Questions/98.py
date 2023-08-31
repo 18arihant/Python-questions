4c112ce391e29429a4735a5d5f8089de4b667592
18arihant <arihant18082003@gmail.com>
2023-08-31 22:11:55 +0530

    Question 98

--- LeetCode Questions/98.py
+++ LeetCode Questions/98.py
@@ -1,1 +1,12 @@
-# 
+# 2828. Check if a String Is an Acronym of Words
+
+class Solution():
+    def isAcronym(self, words, s):
+        ans=""
+        for word in words:
+            ans+=word[0]
+
+        return ans==s  
+
+a=Solution()
+print(a.isAcronym(["an","apple"],'a'))
