e1dc8c4022c4bb6edfb9c8f74bebfaa50cf45d00
18arihant <arihant18082003@gmail.com>
2023-08-31 22:16:40 +0530

    Question 99

--- LeetCode Questions/99.py
+++ LeetCode Questions/99.py
@@ -1,1 +1,13 @@
-# 
+# 2108. Find First Palindromic String in the Array
+
+class Solution():
+    def firstPalindrome(self, words):
+        a=""
+        for i in words:
+            if i==i[::-1]:
+                a=i
+                break
+        return a
+
+a=Solution()
+print(a.firstPalindrome(["abc","car","ada","racecar","cool"]))
